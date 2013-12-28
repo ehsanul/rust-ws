b52db06f91e187f986e5a97978c3826e253da364
Ehsanul Hoque <ehsanul@ehsanul.com>
2013-12-28 14:29:29 -0800

    fix indentation and other minor fixes

--- main.rs
+++ main.rs
@@ -1,4 +1,4 @@
-//! A Web Socket server
+//! A Web Socket Server
 
 #[crate_id = "rust-ws"];
 
@@ -52,53 +52,52 @@ impl Server for WebSocketServer {
     fn handle_request(&self, r: &Request, w: &mut ResponseWriter) {
         // TODO allow configuration of endpoint for websocket
         match (&r.method, &r.headers.upgrade){
-          // (&Get, &Some(~"websocket"), &Some(~[Token(~"Upgrade")])) => { // FIXME this doesn't work. but client must have the header "Connection: Upgrade"
-          (&Get, &Some(~"websocket")) => { // TODO client must have the header "Connection: Upgrade"
-            w.status = SwitchingProtocols;
-            w.headers.upgrade = Some(~"websocket");
+            // (&Get, &Some(~"websocket"), &Some(~[Token(~"Upgrade")])) => { // FIXME this doesn't work. but client must have the header "Connection: Upgrade"
+            (&Get, &Some(~"websocket")) => { // TODO client must have the header "Connection: Upgrade"
+                w.status = SwitchingProtocols;
+                w.headers.upgrade = Some(~"websocket");
 
-            // w.headers.transfer_encoding = None;
-            w.headers.content_length = Some(0);
+                // w.headers.transfer_encoding = None;
+                w.headers.content_length = Some(0);
 
-            w.headers.connection = Some(~[Token(~"Upgrade")]);
+                w.headers.connection = Some(~[Token(~"Upgrade")]);
 
-            // FIXME must we iter?
-            for header in r.headers.iter() {
-              match (header.header_name(), header.header_value()) {
-                (~"Sec-Websocket-Key", val) => {
-                  //  NOTE from RFC 6455
-                  //  As an example, if the value of the |Sec-WebSocket-Key|
-                  //  header field in the client's handshake were
-                  //  "dGhlIHNhbXBsZSBub25jZQ==", the server would append the
-                  //  string "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" to form the
-                  //  string "dGhlIHNhbXBsZSBub25jZQ==258EAFA5-E914-47DA-95CA-
-                  //  C5AB0DC85B11".  The server would then take the SHA-1 hash
-                  //  of this string, giving the value 0xb3 0x7a 0x4f 0x2c 0xc0
-                  //  0x62 0x4f 0x16 0x90 0xf6 0x46 0x06 0xcf 0x38 0x59 0x45
-                  //  0xb2 0xbe 0xc4 0xea.  This value is then base64-encoded,
-                  //  to give the value "s3pPLMBiTxaQ9kYGzzhZRbK+xOo=", which
-                  //  would be returned in the |Sec-WebSocket-Accept| header
-                  //  field.
+                // FIXME must we iter?
+                for header in r.headers.iter() {
+                    match (header.header_name(), header.header_value()) {
+                        (~"Sec-Websocket-Key", val) => {
+                            //  NOTE from RFC 6455
+                            //  As an example, if the value of the |Sec-WebSocket-Key|
+                            //  header field in the client's handshake were
+                            //  "dGhlIHNhbXBsZSBub25jZQ==", the server would append the
+                            //  string "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" to form the
+                            //  string "dGhlIHNhbXBsZSBub25jZQ==258EAFA5-E914-47DA-95CA-
+                            //  C5AB0DC85B11".  The server would then take the SHA-1 hash
+                            //  of this string, giving the value 0xb3 0x7a 0x4f 0x2c 0xc0
+                            //  0x62 0x4f 0x16 0x90 0xf6 0x46 0x06 0xcf 0x38 0x59 0x45
+                            //  0xb2 0xbe 0xc4 0xea.  This value is then base64-encoded,
+                            //  to give the value "s3pPLMBiTxaQ9kYGzzhZRbK+xOo=", which
+                            //  would be returned in the |Sec-WebSocket-Accept| header
+                            //  field.
 
-                  let mut sh = Sha1::new();
-                  let mut out = [0u8, ..20];
-                  sh.input_str(val + WEBSOCKET_SALT);
-                  sh.result(out);
-                  let sec_websocket_accept = out.to_base64(STANDARD);
-                  debug!("sec websocket accept: {}", sec_websocket_accept);
-                  w.headers.insert(ExtensionHeader(~"Sec-WebSocket-Accept", sec_websocket_accept));
+                            let mut sh = Sha1::new();
+                            let mut out = [0u8, ..20];
+                            sh.input_str(val + WEBSOCKET_SALT);
+                            sh.result(out);
+                            let sec_websocket_accept = out.to_base64(STANDARD);
+                            debug!("sec websocket accept: {}", sec_websocket_accept);
+                            w.headers.insert(ExtensionHeader(~"Sec-WebSocket-Accept", sec_websocket_accept));
 
-                  w.headers.date = Some(time::now_utc());
-                  w.headers.server = Some(~"rust-ws/0.0-pre");
+                            w.headers.date = Some(time::now_utc());
+                            w.headers.server = Some(~"rust-ws/0.0-pre");
+                        }
+                        (name, val) => {
+                            debug!("{}: {}", name, val);
+                        }
+                    }
                 }
-                (name, val) => {
-                  debug!("{}: {}", name, val);
-                }
-              }
-            }
-          },
-          (&_, &Some(_)) => {}, // handle other upgrade - this is rare apparently, but may be used for TLS for example. not sure if browsers actually implement it though.
-          (&_, &None) => self.handle_http_request(r, w)
+            },
+            (&_, &_) => self.handle_http_request(r, w)
         }
     }
 }
