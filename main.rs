73473bc1e4390342bb5e86e95f556be784f482d1
Ehsanul Hoque <ehsanul@ehsanul.com>
2013-12-28 17:04:54 -0800

    refactor

--- main.rs
+++ main.rs
@@ -97,6 +97,32 @@ trait WebSocketServer: Server {
         }
     }
 
+    fn sec_websocket_accept(&self, sec_websocket_key: ~str) -> ~str {
+        //  NOTE from RFC 6455
+        // To prove that the handshake was received, the server has to take two
+        // pieces of information and combine them to form a response.  The first
+        // piece of information comes from the |Sec-WebSocket-Key| header field
+        // in the client handshake:
+        //
+        //      Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==
+        //
+        // For this header field, the server has to take the value (as present
+        // in the header field, e.g., the base64-encoded [RFC4648] version minus
+        // any leading and trailing whitespace) and concatenate this with the
+        // Globally Unique Identifier (GUID, [RFC4122]) "258EAFA5-E914-47DA-
+        // 95CA-C5AB0DC85B11" in string form, which is unlikely to be used by
+        // network endpoints that do not understand the WebSocket Protocol.  A
+        // SHA-1 hash (160 bits) [FIPS.180-3], base64-encoded (see Section 4 of
+        // [RFC4648]), of this concatenation is then returned in the server's
+        // handshake.
+
+        let mut sh = Sha1::new();
+        let mut out = [0u8, ..20];
+        sh.input_str(sec_websocket_key + WEBSOCKET_SALT);
+        sh.result(out);
+        return out.to_base64(STANDARD);
+    }
+
     fn handle_possible_ws_request(&self, r: &Request, w: &mut ResponseWriter) {
         // TODO allow configuration of endpoint for websocket
         match (&r.method, &r.headers.upgrade){
@@ -105,44 +131,19 @@ trait WebSocketServer: Server {
                 // WebSocket Opening Handshake
                 w.status = SwitchingProtocols;
                 w.headers.upgrade = Some(~"websocket");
-
                 // w.headers.transfer_encoding = None;
                 w.headers.content_length = Some(0);
-
                 w.headers.connection = Some(~[Token(~"Upgrade")]);
+                w.headers.date = Some(time::now_utc());
+                w.headers.server = Some(~"rust-ws/0.0-pre");
 
                 // FIXME must we iter?
                 for header in r.headers.iter() {
                     match (header.header_name(), header.header_value()) {
                         (~"Sec-Websocket-Key", val) => {
-                            //  NOTE from RFC 6455
-                            // To prove that the handshake was received, the server has to take two
-                            // pieces of information and combine them to form a response.  The first
-                            // piece of information comes from the |Sec-WebSocket-Key| header field
-                            // in the client handshake:
-                            //
-                            //      Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==
-                            //
-                            // For this header field, the server has to take the value (as present
-                            // in the header field, e.g., the base64-encoded [RFC4648] version minus
-                            // any leading and trailing whitespace) and concatenate this with the
-                            // Globally Unique Identifier (GUID, [RFC4122]) "258EAFA5-E914-47DA-
-                            // 95CA-C5AB0DC85B11" in string form, which is unlikely to be used by
-                            // network endpoints that do not understand the WebSocket Protocol.  A
-                            // SHA-1 hash (160 bits) [FIPS.180-3], base64-encoded (see Section 4 of
-                            // [RFC4648]), of this concatenation is then returned in the server's
-                            // handshake.
-
-                            let mut sh = Sha1::new();
-                            let mut out = [0u8, ..20];
-                            sh.input_str(val + WEBSOCKET_SALT);
-                            sh.result(out);
-                            let sec_websocket_accept = out.to_base64(STANDARD);
+                            let sec_websocket_accept = self.sec_websocket_accept(val);
                             debug!("sec websocket accept: {}", sec_websocket_accept);
                             w.headers.insert(ExtensionHeader(~"Sec-WebSocket-Accept", sec_websocket_accept));
-
-                            w.headers.date = Some(time::now_utc());
-                            w.headers.server = Some(~"rust-ws/0.0-pre");
                         }
                         (name, val) => {
                             debug!("{}: {}", name, val);
