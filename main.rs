8852f962bd3336213143354fd19d56ec043ecf3b
Ehsanul Hoque <ehsanul@ehsanul.com>
2014-02-09 22:56:44 -0800

    update for latest rust + write to websocket

--- main.rs
+++ main.rs
@@ -6,7 +6,8 @@ extern mod extra;
 extern mod http;
 extern mod rust_crypto = "rust-crypto";
 
-use std::str::from_utf8;
+use std::io::IoResult;
+use std::str;
 
 use rust_crypto::sha1::Sha1;
 use rust_crypto::digest::Digest;
@@ -16,9 +17,7 @@ use std::io::net::ip::{SocketAddr, Ipv4Addr};
 use std::io::Writer;
 use extra::time;
 
-use std::comm::SharedChan;
 use std::io::{Listener, Acceptor};
-use std::io::io_error;
 use std::io::net::tcp::TcpListener;
 
 use http::buffer::BufferedStream;
@@ -34,10 +33,6 @@ use http::method::Get;
 
 static WEBSOCKET_SALT: &'static str = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
-enum WebSocketState {
-  WSConnecting, WSOpen, WSClosing, WSClosed
-}
-
 trait WebSocketServer: Server {
 
     // this is mostly a copy of the serve_forever fn in the Server trait
@@ -45,35 +40,29 @@ trait WebSocketServer: Server {
         let config = self.get_config();
         debug!("About to bind to {:?}", config.bind_address);
         let mut acceptor = match TcpListener::bind(config.bind_address).listen() {
-            None => {
-                error!("bind or listen failed :-(");
+            Err(err) => {
+                error!("bind or listen failed :-(: {}", err);
                 return;
             },
-            Some(acceptor) => acceptor,
+            Ok(acceptor) => acceptor,
         };
         debug!("listening");
         loop {
-            // OK, we're sort of shadowing an IoError here. Perhaps this should be done in a
-            // separate task so that it can safely fail...
-            let mut error = None;
-            let optstream = io_error::cond.trap(|e| {
-                error = Some(e);
-            }).inside(|| {
-                acceptor.accept()
-            });
-
-            if optstream.is_none() {
-                debug!("accept failed: {:?}", error);
-                // Question: is this the correct thing to do? We should probably be more
-                // intelligent, for there are some accept failures that are likely to be
-                // permanent, such that continuing would be a very bad idea, such as
-                // ENOBUFS/ENOMEM; and some where it should just be ignored, e.g.
-                // ECONNABORTED. TODO.
-                continue;
-            }
+            let stream = match acceptor.accept() {
+                Err(error) => {
+                    debug!("accept failed: {:?}", error);
+                    // Question: is this the correct thing to do? We should probably be more
+                    // intelligent, for there are some accept failures that are likely to be
+                    // permanent, such that continuing would be a very bad idea, such as
+                    // ENOBUFS/ENOMEM; and some where it should just be ignored, e.g.
+                    // ECONNABORTED. TODO.
+                    continue;
+                },
+                Ok(socket) => socket,
+            };
             let child_self = self.clone();
-            do spawn {
-                let mut stream = BufferedStream::new(optstream.unwrap());
+            spawn(proc() {
+                let mut stream = BufferedStream::new(stream);
                 debug!("accepted connection, got {:?}", stream);
 
                 let mut successful_handshake = false;
@@ -84,7 +73,13 @@ trait WebSocketServer: Server {
                         Ok(()) => {
                             successful_handshake = child_self.handle_possible_ws_request(request, response);
                             // Ensure that we actually do send a response:
-                            response.try_write_headers();
+                            match response.try_write_headers() {
+                                Err(err) => {
+                                    error!("Writing headers failed: {}", err);
+                                    return;  // Presumably bad connection, so give up.
+                                },
+                                Ok(_) => (),
+                            }
                         },
                         Err(status) => {
                             // Uh oh, it's a response that I as a server cannot cope with.
@@ -92,11 +87,23 @@ trait WebSocketServer: Server {
                             // at least I am content to send no body in the response.
                             response.status = status;
                             response.headers.content_length = Some(0);
-                            response.write_headers();
+                            match response.write_headers() {
+                                Err(err) => {
+                                    error!("Writing headers failed: {}", err);
+                                    return;  // Presumably bad connection, so give up.
+                                },
+                                Ok(_) => (),
+                            }
                         },
                     }
                     // Ensure the request is flushed, any Transfer-Encoding completed, etc.
-                    response.finish_response();
+                    match response.finish_response() {
+                        Err(err) => {
+                            error!("finishing response failed: {}", err);
+                            return;  // Presumably bad connection, so give up.
+                        },
+                        Ok(_) => (),
+                    }
 
                     if successful_handshake || request.close_connection {
                         break;
@@ -104,38 +111,73 @@ trait WebSocketServer: Server {
                 }
 
                 if successful_handshake {
-                    child_self.serve_websockets(&mut stream);
+                    child_self.serve_websockets(stream);
                 }
-            }
+            });
         }
     }
 
-    fn serve_websockets(&self, stream: &mut BufferedStream<TcpStream>) {
-        let mut status = WSOpen;
+    fn serve_websockets(&self, stream: BufferedStream<TcpStream>) -> IoResult<uint> {
+        let mut stream = stream.wrapped;
+        let write_stream = stream.clone();
+        let (port, chan) = Chan::new();
+        chan.send(~"test payload");
+        spawn(proc() {
+            // ugh: https://github.com/mozilla/rust/blob/3dbc1c34e694f38daeef741cfffc558606443c15/src/test/run-pass/kindck-implicit-close-over-mut-var.rs#L40-L44
+            // work to fix this is ongoing here: https://github.com/mozilla/rust/issues/11958
+            let mut write_stream = write_stream;
+
+            // we're ignoring all possible write errors for now. need a macro
+            // that shuts this task down (if_ok! just returns the Err(e), which
+            // is no good since the proc type has to return () to be used for
+            // task spawning
+            loop {
+                let payload = port.recv();
+                write_stream.write_u8(0b1000_0001); // fin: 1, rsv: 000, opcode: 0001 (text frame) - TODO make this a configurable option
+                let payload = "From Rust: " + payload;
+                let payload_length = payload.len(); // FIXME len() returns a uint, so i'm guessing this doesn't work for extremely large payloads. in ws, payload length itself may be upto 64 bits. ie a 2gb+ message fails
+
+                // the first bit, which is the "mask" bit, is implicitly set as 0 here, as required for ws servers
+                if payload_length <= 125 {
+                    write_stream.write_u8(payload_length as u8);
+                } else if payload_length <= 65536 {
+                    write_stream.write_u8(126);
+                    write_stream.write_be_u16(payload_length as u16);
+                } else if payload_length <= 65536 {
+                    write_stream.write_u8(127);
+                    write_stream.write_be_u64(payload_length as u64);
+                }
+
+                write_stream.write_str(payload);
+                write_stream.flush();
+            }
+        });
         loop {
-            let buf1 = stream.read_bytes(2); // FIXME trap io_error condition
+            let buf1 = if_ok!(stream.read_bytes(2));
             debug!("buf1: {:t} {:t}", buf1[0], buf1[1]);
 
             let fin    = buf1[0] & 0b1000_0000; // TODO check this, required for handling fragmented messages
+            /* we ignore these, as they are only used if a websocket protocol has been enabled, and optionally at that
             let rsv1   = buf1[0] & 0b0100_0000;
             let rsv2   = buf1[0] & 0b0010_0000;
             let rsv3   = buf1[0] & 0b0001_0000;
-            let opcode = buf1[0] & 0b0000_1111;
+            */
+            let opcode = buf1[0] & 0b0000_1111; // TODO check for ping/pong/text/binary
 
             let mask    = buf1[1] & 0b1000_0000;
             let pay_len = buf1[1] & 0b0111_1111;
 
             let payload_length = match pay_len {
-                127 => stream.read_be_u64(), // 8 bytes in network byte order
-                126 => stream.read_be_u16() as u64, // 2 bytes in network byte order
+                127 => if_ok!(stream.read_be_u64()), // 8 bytes in network byte order
+                126 => if_ok!(stream.read_be_u16()) as u64, // 2 bytes in network byte order
                 _   => pay_len as u64
             };
             debug!("payload_length: {}", payload_length);
 
-            let masking_key_buf = stream.read_bytes(4);
+            let masking_key_buf = if_ok!(stream.read_bytes(4));
             debug!("masking_key_buf: {:t} {:t} {:t} {:t}", masking_key_buf[0], masking_key_buf[1], masking_key_buf[2], masking_key_buf[3]);
 
-            let masked_payload_buf = stream.read_bytes(payload_length as uint); // FIXME payload_length could be upto 64 bits, so this could fail on archs with a 32-bit uint
+            let masked_payload_buf = if_ok!(stream.read_bytes(payload_length as uint)); // FIXME payload_length could be upto 64 bits, so this could fail on archs with a 32-bit uint
 
             // unmask the payload
             let mut payload_buf = ~[]; // ugh, a map_with_index would be nice. or maybe just mutate the existing buffer in place.
@@ -143,8 +185,9 @@ trait WebSocketServer: Server {
                 payload_buf.push(octet ^ masking_key_buf[i % 4]);
             }
 
-            let payload = from_utf8(payload_buf); // FIXME could be text OR binary! look at opcode to know which
+            let payload = str::from_utf8_owned(payload_buf).unwrap(); // FIXME shouldn't just unwrap? also, could be text OR binary! look at opcode to know which
             debug!("payload: {}", payload);
+            chan.send(payload);
         }
     }
 
